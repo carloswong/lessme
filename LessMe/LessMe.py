78316fdbb78110a116d807a773d018893a56f8dc
carloswong <2carlos.wong@gmail.com>
2014-06-20 18:27:00 +0800

    add support for import tag

--- LessMe/LessMe.py
+++ LessMe/LessMe.py
@@ -6,31 +6,50 @@ class LessMe(sublime_plugin.EventListener):
 		syntaxPath = view.settings().get('syntax')
 		compressed = filepath = ext = syntax = ""
 
-		if filename:
+		if not filename:
+			return
+		else:	
 			filepath,exts = os.path.splitext(filename)
 			ext = exts[1:]
-			compressed = filepath + '.css'
-			
+
 		if syntaxPath:
 			syntax = os.path.splitext(syntaxPath)[0].split('/')[-1].lower()
 
 		if ext in ['less'] or 'less' in [syntax] :
-			self.compile_to_css(filename, compressed, view)
+			line = view.substr(view.line(1))
+			tag = "//@module"
+			moudles = [];
+			if line.startswith(tag):
+				moudles = (line[len(tag):-1]).split()
 
+			if len(moudles):
+				current_dir = os.path.dirname(filename)
+				for module in moudles :
+					origin = os.path.realpath(os.path.join(current_dir,module))
+					if(os.path.exists(origin)):
+						compiled = self.get_compiled_file_name(origin)
+						self.compile_to_css(origin, compiled)
+			else:
+				compressed = filepath + '.css'
+				self.compile_to_css(filename, compressed)
 
-	def compile_to_css(self, source, target, view):
+	def get_compiled_file_name(self,filename):
+		filepath,exts = os.path.splitext(filename)
+		return filepath + '.css'
+
+	def compile_to_css(self, source, target):
 		cmd = " ".join(['lessc','"'+source+'"','"'+target+'"', '--source-map','--no-color'])
 		pipe = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
 		error = pipe.stderr.read()
 		if error:
-			message = '[LESS Compile Fail]\n-------------------------\n'
+			message = '[LESS编译失败]\n-------------------------\n'
 			try:
 				message += error.decode()
 			except UnicodeDecodeError:
 				message += error.decode('gb2312')
 			sublime.error_message(message+'-------------------------')
 		else:
-			sublime.status_message('[LESS Compile Success at: '+target+']')
+			sublime.status_message('[LESS 编译成功: '+target+']')
 
 
 
--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 LessMe
 ======
-A Sublime Text 3 plugin for automatically compiling .less file to .css as well as source map file when it be saved.
+A Sublime Text 3 plugin for automatically compiling .less file to .css as well as source map file when it be saved.  
 
+
+import tag supported
+----------
+When importing a another less file by using **import** tag in main.less, just like this:
+```less
+@import url("submodule.less")
+...
+```
+and you should add the below code in the submodule.less's **first line**.
+```
+//@module ..main.less
+```
+
+And then, when the submodule.less be saved, the main.less will be compiled to css as well.  
 
 Dependencies
 ----------
